cb769e41150f28aa0d976fd3128d56e312a01692
arik30000 <arik30000@gmail.com>
2019-07-01 13:45:35 +0300

    make it better

--- staticserverrun.js
+++ staticserverrun.js
@@ -36,7 +36,7 @@ db.once('open', function(callback){
 app.post('/log-in',function(req,res){
   if(req.body.usrname=="admin@gmail.com" && req.body.psw=="Admin1!")
         {
-          res.sendFile(path.join(__dirname + "/index.html"));  
+          res.redirect("/index"); 
         }
         else
         {
@@ -45,7 +45,7 @@ app.post('/log-in',function(req,res){
             if(user ===null){
               res.end("Login invalid");
            }else if (user.name === req.body.usrname && user.password === req.body.psw){
-            res.sendFile(path.join(__dirname + "/index.html")); 
+            res.redirect("/index");  
          } else {
            console.log("Credentials wrong");
            res.end("Login invalid");
